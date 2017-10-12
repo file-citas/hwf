b50c177d2e09f18bbf9706b92b0e413d6b372b83
Dokyung Song <dokyung.song@gmail.com>
2017-10-11 21:21:04 -0700

    Shared memory write analysis (WIP)

--- driver/llvm/lib/Analysis/FirmwareInterface.cpp
+++ driver/llvm/lib/Analysis/FirmwareInterface.cpp
@@ -1,23 +1,67 @@
 #include "llvm/Pass.h"
+#include "llvm/ADT/SmallSet.h"
 #include "llvm/IR/Function.h"
-#include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/IR/IntrinsicInst.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/TypeFinder.h"
 
 using namespace llvm;
 
 namespace {
-struct FirmwareInterface : public FunctionPass {
+struct SharedMemoryWrite : public ModulePass {
   static char ID;
-  FirmwareInterface() : FunctionPass(ID) {}
+  SharedMemoryWrite() : ModulePass(ID) {}
 
-  bool runOnFunction(Function &F) override {
-    errs() << "Firmware Interface: ";
-    errs().write_escaped(F.getName()) << '\n';
+  // TODO: Inter-procedural
+  StructType *getAllocatedType(Value* V) {
+    Type *Ty = V->getType();
+    while (CastInst *CI = dyn_cast<CastInst>(V)) {
+      V = CI->getOperand(0);
+      Ty = V->getType();
+    }
+    return dyn_cast<StructType>(Ty);
+  }
+
+  bool runOnModule(Module &M) override {
+    TypeFinder TF;
+    TF.run(M, true /* onlyNamed */);
+
+    SmallSet<StructType *, 16> SharedTypes;
+    for (StructType* STy : TF) {
+      if (STy->getName().startswith("struct.hfi_cmd")) {
+        SharedTypes.insert(STy);
+      }
+      STy->dump();
+    }
+
+    for (Function &F: M) {
+      for (BasicBlock &B: F) {
+        for (Instruction &I : B) {
+          Value *V = nullptr;
+          if (MemTransferInst *MTI = dyn_cast<MemTransferInst>(&I)) {
+            V = MTI->getRawSource();
+          }
+          else if (StoreInst* SI = dyn_cast<StoreInst>(&I)) {
+            V = SI->getValueOperand();
+          }
+
+          if (V != nullptr) {
+            if (StructType *STy = getAllocatedType(V)) {
+              if (SharedTypes.count(STy) > 0) {
+                I.dump();
+              }
+            }
+          }
+        }
+      }
+    }
     return false;
   }
 };
 } //end of anonymous namespace
 
-char FirmwareInterface::ID = 0;
-static RegisterPass<FirmwareInterface> X("Firmware Interface", "Firmware Interface Pass",
+char SharedMemoryWrite::ID = 0;
+static RegisterPass<SharedMemoryWrite> X("smem-write", "Shared Memory Write Analysis Pass",
                                          false /* Only looks at CFG */,
-                                         false /* Analysis Pass */);
+                                         true /* Analysis Pass */);
---
+++ driver/llvm/lib/README.md
@@ -0,0 +1,4 @@
+### How to run an LLVM pass
+```
+opt -load LLVMFirmwareInterface.so -smem-write < vidc.bc > /dev/null
+```
